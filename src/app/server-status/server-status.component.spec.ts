acd32367ce5ae86ca834fe9d78d1538e6c714a49
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-01-02 16:56:09 +1100

    Angular 11

--- src/app/server-status/server-status.component.spec.ts
+++ src/app/server-status/server-status.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ServerStatusComponent } from './server-status.component';
 
@@ -6,7 +6,7 @@ describe('ServerStatusComponent', () => {
   let component: ServerStatusComponent;
   let fixture: ComponentFixture<ServerStatusComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ServerStatusComponent]
     })
